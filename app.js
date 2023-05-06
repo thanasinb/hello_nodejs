f3d1f6496d96e417baaa0e0cfdd59a8ae9db918d
thanasinb <55657279+thanasinb@users.noreply.github.com>
2023-05-06 13:15:37 +0700

    update host ip

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
-// const hostname = '20.231.75.176';
-const hostname = '127.0.0.7';
+const hostname = '0.0.0.0';
 const port = 3001;
 
 const server = http.createServer((req, res) => {
